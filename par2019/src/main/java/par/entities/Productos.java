d55636487a40e8651df9025cd84e24711b102803
Unknown <prezgonzlez.por@gmail.com>
2019-03-09 19:36:18 -0300

    productos

---
+++ par2019/src/main/java/par/entities/Productos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package par.entities;
+
+/**
+ *
+ * @author Perez
+ */
+public class Productos {
+    Long idProducto;
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+    
+}
