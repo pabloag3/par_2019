8e1f8e3954df67258a38f6f4be6748628cdd842f
Pablo <tatoaguilar95@gmail.com>
2019-03-09 19:39:33 -0300

    Categorias

---
+++ par2019/src/main/java/par/entities/Categorias.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package par.entities;
+
+/**
+ *
+ * @author PabloAgHP
+ */
+public class Categorias {
+    Long idCategoria;
+    String descripcion;
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
