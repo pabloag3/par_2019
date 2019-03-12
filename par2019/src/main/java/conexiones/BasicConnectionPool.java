731245e77734193c208c874277ab47deca3f87dc
Pablo <tatoaguilar95@gmail.com>
2019-03-11 21:13:37 -0300

    Conexion a la bd

---
+++ par2019/src/main/java/conexiones/BasicConnectionPool.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexiones;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author PabloAgHP
+ */
+public class BasicConnectionPool implements ConnectionPool {
+ 
+    private String url;
+    private String user;
+    private String password;
+    private List<Connection> connectionPool;
+    private List<Connection> usedConnections = new ArrayList<>();
+    private static int INITIAL_POOL_SIZE = 10;
+    
+    private BasicConnectionPool(String url, String user, String password, List<Connection> pool) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        this.connectionPool = pool;
+    }
+     
+    public static BasicConnectionPool create(
+      String url, String user, 
+      String password) throws SQLException {
+  
+        List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
+        for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
+            pool.add(createConnection(url, user, password));
+        }
+        return new BasicConnectionPool(url, user, password, pool);
+    }
+     
+    // standard constructors
+     
+    @Override
+    public Connection getConnection() {
+        Connection connection = connectionPool
+          .remove(connectionPool.size() - 1);
+        usedConnections.add(connection);
+        return connection;
+    }
+     
+    @Override
+    public boolean releaseConnection(Connection connection) {
+        connectionPool.add(connection);
+        return usedConnections.remove(connection);
+    }
+     
+    private static Connection createConnection(
+      String url, String user, String password) 
+      throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+     
+    public int getSize() {
+        return connectionPool.size() + usedConnections.size();
+    }
+ 
+    // standard getters
+    @Override
+    public String getUrl() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getUser() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPassword() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ par2019/src/main/java/conexiones/app.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexiones;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import par.entities.Categorias;
+
+/**
+ *
+ * @author PabloAgHP
+ */
+public class app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        Connection conexion = null;
+        try {
+            conexion = BasicConnectionPool.create("jdbc:postgresql://localhost:5432/ecomerce", "postgres", "QAZwsx123plm").getConnection();
+            Statement sentencia = conexion.createStatement();
+            //String sql = "SELECT * FROM CATEGORIA";
+            String sql = "insert into CATEGORIA values (2, '{domitica}');";
+            ResultSet resultado = sentencia.executeQuery(sql);
+            System.out.println("resultado " + resultado);
+        } catch (Exception e) {
+            System.out.println("error");
+        } finally {
+            //conexion.close();
+        };
+            
+            
+    }
+    
+}
