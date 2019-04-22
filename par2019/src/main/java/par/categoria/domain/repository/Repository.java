8be2745daeff74cf784ea82e0ea3aedcc1611dff
Unknown <prezgonzlez.por@gmail.com>
2019-04-21 20:32:01 -0400

    actualizacion rest pai categoria

--- par2019/src/main/java/par/categoria/domain/repository/Repository.java
+++ par2019/src/main/java/par/categoria/domain/repository/Repository.java
@@ -13,18 +13,21 @@ public interface Repository<TE, T> extends ReadOnlyRepository<TE, T> {
     /**
      *
      * @param entity
+     * @throws java.lang.Exception
      */
-    void add(TE entity);
+    void add(TE entity) throws Exception;
 
     /**
      *
      * @param id
+     * @throws java.lang.Exception
      */
-    void remove(T id);
+    void remove(T id) throws Exception;
 
     /**
      *
      * @param entity
+     * @throws java.lang.Exception
      */
-    void update(TE entity);
+    void update(TE entity) throws Exception;
 }
